9bad99d3175351b38ff85acd092b8d740c2cc57b
wchaney98 <wills.apps@yahoo.com>
2016-02-22 19:40:04 -0700

    DevLog 2/22
    Began work on the main game loop (quicktime object) by struggling with
    the animation stuff but finally figured it out. Still fleshing out the
    concepts besides the main game loop

---
+++ Quicktime Fishing/Assets/Scripts/CountdownAnim.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CountdownAnim : MonoBehaviour
+{
+
+    Animator animPlayer;
+
+    float threeTime;
+    float twoTime;
+    float oneTime;
+
+    float timeElapsedSinceSpriteChanged;
+
+    byte currentNum;
+
+    Dictionary<byte, float> timeLinkDict = new Dictionary<byte, float>();
+
+    void Start()
+    {
+        animPlayer = GetComponent<Animator>();
+
+        threeTime = 0f;
+        twoTime = 0.5f;
+        oneTime = 0.9f;
+
+        timeElapsedSinceSpriteChanged = 0f;
+
+        currentNum = 3;
+
+        timeLinkDict.Add(3, threeTime);
+        timeLinkDict.Add(2, twoTime);
+        timeLinkDict.Add(1, oneTime);
+    }
+
+    void Update()
+    {
+        timeElapsedSinceSpriteChanged += Time.deltaTime;
+
+        if (timeElapsedSinceSpriteChanged >= 1f)
+        {
+            countDown();
+            timeElapsedSinceSpriteChanged = 0;
+        }
+    }
+
+    void countDown()
+    {
+        currentNum -= 1;
+        if (currentNum <= 0)
+            Destroy(transform.parent.gameObject);
+        animPlayer.Play("321CountDown", 0, timeLinkDict[currentNum]); 
+    }
+}
